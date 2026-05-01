e017c692321defa91317db6fc050875abbb41cd9
gynch0 <106382132+gynch0@users.noreply.github.com>
2026-05-01 22:16:58 +0400

    Delete js directory

--- js/burger.js
+++
@@ -1,7 +0,0 @@
-const burger = document.querySelector('.burger');
-const nav = document.querySelector('.header__nav');
-
-burger.addEventListener('click', () => {
-  burger.classList.toggle('burger--active');
-  nav.classList.toggle('header__nav--open');
-});
